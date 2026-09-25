0afc1196705def1d04011cc20507c0ca50c72300
lucasg24987-cmyk <lucasg24987@gmail.com>
2026-09-25 13:27:24 -0300

    Create Si.js

---
+++ Si.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'tetris-maple-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/833/833316.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
